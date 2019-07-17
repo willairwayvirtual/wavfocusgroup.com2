f59e93bec08b6a3727ba36ad357ae08d26bf183a
Will Jones <willjones97@hotmail.com>
2019-07-17 10:44:00 +0100

    grkrtwjedeth45tp1e

--- wavfocusgroup.com2/wavfocusgroup.aspx.vb
+++ wavfocusgroup.com2/wavfocusgroup.aspx.vb
@@ -2,4 +2,7 @@
 Partial Class wavfocusgroup
     Inherits System.Web.UI.Page
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Response.Redirect("login.aspx")
+    End Sub
 End Class
